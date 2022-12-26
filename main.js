90102e620dd37ac23bc09086faa6d214010dda3a
WalobwaD <walobwadan@gmail.com>
2022-12-26 16:34:39 +0300

    Optimization

--- main.js
+++ main.js
@@ -8,41 +8,68 @@ import stars2 from "./src/stars2.jpg"
 import stars3 from "./src/stars3.jpg"
 
 
-
+let loader = document.getElementById("pre-loader")
 const canvas = document.querySelector(".webGL")
 let sizes = {
   width : window.innerWidth,
   height : window.innerHeight,
 }
 
+window.addEventListener("load", function(){
+    loader.style.display = "none"
+})
 
 //creating a scene, renderer, camera, mesh(geometry and material)
 const scene = new THREE.Scene()
+
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 18
+
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize( sizes.width, sizes.height ); 
+renderer.setSize( sizes.width, sizes.height );
+
+
 const textureLoader = new THREE.TextureLoader()
-scene.background = textureLoader.load(stars1)
+scene.background = textureLoader.load(stars2)
 
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: "white",
-  roughness: 0.1
+  roughness: 0.1,
+  map : textureLoader.load(stars1)
 })
 const sphere = new THREE.Mesh(geometry, material)
 
+function addStar () {
+  const geometry = new THREE.SphereGeometry(0.07)
+  const material = new THREE.MeshStandardMaterial({
+    color : 0xffffff,
+  })
+  const stars = new THREE.Mesh(geometry, material)
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ))
+
+  stars.position.set(x, y, z)
+  scene.add(stars)
+
+}
+Array(2500).fill().forEach(addStar)
+
+//adding light
 const light = new THREE.PointLight(0xffffff, 1, 100)
-light.position.set(1, 1, 20)
+light.position.set(10, 10, 10)
+const ambient = new THREE.AmbientLight(0xffffff)
 
 scene.add(sphere)
-scene.add(light)
+scene.add(light, ambient)
 renderer.render(scene, camera)
 
+
+//allowing mouse controls on page
 let controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.autoRotate = true
-controls.autoRotateSpeed = 5
+controls.autoRotateSpeed = 2
 
 window.addEventListener("resize", ()=> {
   sizes.width = window.innerWidth
@@ -54,9 +81,6 @@ window.addEventListener("resize", ()=> {
 })
 
 let loop = () => {
-  // sphere.position.x +=0.1
-  // sphere.position.y +=0.1
-  // sphere.position.z +=0.1
   controls.update()
   renderer.render(scene, camera)
   renderer.setPixelRatio(2)
@@ -66,7 +90,14 @@ loop()
 
 const animations = gsap.timeline({defaults: { duration: 1}})
 animations.fromTo(sphere.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
-animations.fromTo(".title", {opacity: 0}, {opacity: 1})
+animations.fromTo(".intro-outline", {opacity: 0}, {opacity: 0.7})
+animations.fromTo(".greet", {opacity: 1}, {opacity: 0})
+
+animations.fromTo(".message", {visibility: 'hidden'}, {duration:9 ,visibility: 'visible'})
+animations.fromTo('.info',{opacity: 0}, {opacity: 0.8})
+animations.to(".message", {duration:9, visibility: 'hidden'})
+
+
 
 let mouseDown = false
 let rgb = []
@@ -89,4 +120,6 @@ window.addEventListener('mousemove', (e) => {
     })
   }
 })
+
+
 
